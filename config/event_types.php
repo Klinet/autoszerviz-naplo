2ff4fbc70843211879adf8e0a0a0fe1229c6c7f9
Klinet <akali.klinet@gmail.com>
2025-03-25 13:02:00 +0100

    [dev] configs database_path & event_types

---
+++ config/event_types.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+    'types' => [
+        'regisztralt' => [
+            'id' => 1,
+            'name' => 'Regisztrált',
+        ],
+        'kotelezo_szerviz' => [
+            'id' => 2,
+            'name' => 'Kötelező szerviz',
+        ],
+        'javitas' => [
+            'id' => 3,
+            'name' => 'Javítás',
+        ],
+        'kiregisztralt' => [
+            'id' => 4,
+            'name' => 'Kiregisztrált',
+        ],
+        'javithatatlan' => [
+            'id' => 5,
+            'name' => 'Javíthatatlan',
+        ],
+        'visszahivasrol_ertesitve' => [
+            'id' => 6,
+            'name' => 'Visszahívásról értesítve',
+        ],
+        'visszahivasra_idopontot_kert' => [
+            'id' => 7,
+            'name' => 'Visszahívásra időpontot kért',
+        ],
+        'visszahivasra_idopontot_modositott' => [
+            'id' => 8,
+            'name' => 'Visszahívásra időpontot módosított',
+        ],
+        'visszahivasra_jelentkezett' => [
+            'id' => 9,
+            'name' => 'Visszahívásra jelentkezett',
+        ],
+        'kotelezo_szervizre_felszolitva' => [
+            'id' => 10,
+            'name' => 'Kötelező szervizre felszólítva',
+        ],
+        'kotelezo_szervizre_idopontot_kert' => [
+            'id' => 11,
+            'name' => 'Kötelező szervizre időpontot kért',
+        ],
+        'kotelezo_szervizre_idopontot_modositott' => [
+            'id' => 12,
+            'name' => 'Kötelező szervizre időpontot módosított',
+        ],
+        'kotelezo_szervizre_megjelent' => [
+            'id' => 13,
+            'name' => 'Kötelező szervizre megjelent',
+        ],
+        'kotelezo_szervizre_2szor_felszolitva' => [
+            'id' => 14,
+            'name' => 'Kötelező szervizre 2x felszólítva',
+        ],
+        'meghibasodas' => [
+            'id' => 15,
+            'name' => 'Meghibásodás',
+        ],
+        'javitas_megkezdve' => [
+            'id' => 16,
+            'name' => 'Javítás megkezdve',
+        ],
+        'javitas_elkeszult' => [
+            'id' => 17,
+            'name' => 'Javítás elkészült',
+        ],
+    ],
+];
