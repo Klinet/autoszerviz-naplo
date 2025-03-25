61f3f7f8f3b7d227afb449d5a36418dbcb1611eb
Klinet <akali.klinet@gmail.com>
2025-03-25 22:35:05 +0100

    [fix] StoreServiceLogRequest

--- app/Domains/ServiceLog/Http/Requests/StoreServiceLogRequest.php
+++ app/Domains/ServiceLog/Http/Requests/StoreServiceLogRequest.php
@@ -7,22 +7,15 @@ use Illuminate\Validation\Rule;
 
 class StoreServiceLogRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
+            'client_id' => 'required|integer|exists:clients,id',
             'car_id' => 'required|integer|exists:cars,id',
             'lognumber' => [
                 'required',
