8d607cb44ed5aaef9e9311093b2fa451cdc9f768
Klinet <akali.klinet@gmail.com>
2025-03-25 14:48:20 +0100

    [dev] Resources & Controllers

---
+++ app/Domains/Owner/Http/Controllers/OwnerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domains\Owner\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OwnerController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
---
+++ app/Domains/ServiceLog/Http/Controllers/ServiceLogController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domains\ServiceLog\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ServiceLogController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
